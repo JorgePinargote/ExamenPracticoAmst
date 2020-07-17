c0bdeb7b88a0154227abed5bbf3182258a9e2ba7
elibemar <elibemar@espol.edu.ec>
2020-07-17 12:13:24 -0500

    activities

---
+++ app/src/main/java/com/example/examenpractico/PerdedorActivity.java
@@ -0,0 +1,36 @@
+package com.example.examenpractico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class PerdedorActivity extends AppCompatActivity {
+    TextView txtAciertos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perdedor);
+        String valor = getIntent().getExtras().getString("aciertos");
+        txtAciertos = (TextView)findViewById(R.id.text_inicio);
+        txtAciertos.setText("Ha respondido " + valor + " preguntas");
+
+    }
+
+    public void salir(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void JugarNuevamente(View v){
+
+        /////REEMPLAZAR CON ACTIVITY TRIVIA/////////////////////////////////////////////////////////////
+        Intent intent = new Intent(this, GanadorActivity.class);
+        startActivity(intent);
+    }
+}
